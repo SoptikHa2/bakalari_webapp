8ff9158ddb9185cf232785e58182d1909aabc0f0
Petr Stastny <petr.stastny01@gmail.com>
2019-01-14 20:51:39 +0100

    Another subject with no grades hotfix

--- web/webapp/controller/student/subject.dart
+++ web/webapp/controller/student/subject.dart
@@ -10,54 +10,64 @@ import '../../view/student/subjectListView.rsp.dart';
 /// This controllers handles both
 /// one subject details lookup
 /// and subject list
-/// 
+///
 /// /student/subject -> display subject list
 /// /student/subject/XX -> display subject XX
 class Subject {
-  static Future getList(HttpConnect connect) async{
+  static Future getList(HttpConnect connect) async {
     var result = await Tools.loginAsStudent(connect.request.cookies);
     ComplexStudent student = null;
     if (result.success) {
       student = result.result;
-    }else{
-      if(result.requestLogout){
+    } else {
+      if (result.requestLogout) {
         return connect.redirect('/logout');
-      }else{
+      } else {
         return connect.redirect('/?error=not_logged_in');
       }
     }
 
     Map<String, double> averages = null;
     if (student.grades != null && student.subjects != null) {
-      averages = Tools.gradesToSubjectAverages(student.grades, student.subjects);
+      averages =
+          Tools.gradesToSubjectAverages(student.grades, student.subjects);
     }
 
-    return subjectListView(connect, subjects: student.subjects, grades: averages);
+    return subjectListView(connect,
+        subjects: student.subjects, grades: averages);
   }
 
-  static Future getSubject(HttpConnect connect) async{
+  static Future getSubject(HttpConnect connect) async {
     String identifier = Uri.decodeComponent(connect.dataset['identifier']);
     var result = await Tools.loginAsStudent(connect.request.cookies);
     ComplexStudent student = null;
     if (result.success) {
       student = result.result;
-    }else{
-      if(result.requestLogout){
+    } else {
+      if (result.requestLogout) {
         return connect.redirect('/logout');
-      }else{
+      } else {
         return connect.redirect('/?error=not_logged_in');
       }
     }
-    if(!student.subjects.any((s) => s.subjectLong == identifier)){
+    if (!student.subjects.any((s) => s.subjectLong == identifier)) {
       throw new Http404(connect.request.uri.toString());
     }
-    
+
     Map<String, double> averages = null;
     if (student.grades != null && student.subjects != null) {
-      averages = Tools.gradesToSubjectAverages(student.grades, student.subjects);
+      averages =
+          Tools.gradesToSubjectAverages(student.grades, student.subjects);
     }
-    
+
     var sub = student.subjects.firstWhere((s) => s.subjectLong == identifier);
-    return subjectDetailsView(connect, subject: sub, grades: student.grades.where((g) => g.subject == sub.subjectShort), prumer: averages[sub.subjectLong].toStringAsFixed(2));
+    String prumer = "-";
+    if (averages.containsKey(sub.subjectLong)) {
+      prumer = averages[sub.subjectLong].toStringAsFixed(2);
+    }
+    return subjectDetailsView(connect,
+        subject: sub,
+        grades: student.grades.where((g) => g.subject == sub.subjectShort),
+        prumer: prumer);
   }
 }
