3fc980e56c28f8719cac1dc897f0056996ddc865
Petr Šťastný <petr.stastny01@gmail.com>
2019-01-31 18:27:11 +0100

    Updated grade display

--- web/js/subjectDetails.js
+++ web/js/subjectDetails.js
@@ -1,34 +1,36 @@
 // Show and update subject average
 
-function updateSubjectAverage(){
+function updateSubjectAverage() {
     let values = document.getElementsByName('value');
     let weights = document.getElementsByName('weight');
     let sum = 0.0;
     let count = 0;
     for (let i = 0; i < values.length; i++) {
-        let value = values[i].innerText;      
+        let value = values[i].innerText;
         let weight = weights[i].innerText;
 
-        if(value.endsWith('-')){
-            value = parseFloat(value) + 0.5;
-        }else{
-            value = parseFloat(value);
+        if (!isNaN(parseFloat(value))) {
+            if (value.endsWith('-')) {
+                value = parseFloat(value) + 0.5;
+            } else {
+                value = parseFloat(value);
+            }
+            weight = parseInt(weight);
+
+
+            sum += value * weight;
+            count += weight;
         }
-        weight = parseInt(weight);
-
-
-        sum += value * weight;
-        count += weight;
     }
-    
+
     let newValue = document.getElementById('new-grade').value;
     let newWeight = parseFloat(document.getElementById('new-weight').value);
-    if(newValue.endsWith('-')){
+    if (newValue.endsWith('-')) {
         newValue = parseFloat(newValue) + 0.5;
-    }else{
+    } else {
         newValue = parseFloat(newValue);
     }
-    if( !isNaN(newValue) && !isNaN(newWeight) && newValue >= 1 && newValue <= 5){
+    if (!isNaN(newValue) && !isNaN(newWeight) && newValue >= 1 && newValue <= 5) {
         sum += newValue * newWeight;
         count += newWeight;
     }
