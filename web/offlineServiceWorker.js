019acbfed99c9e0a37d64767fbad42264c3e3610
Petr Šťastný <petr.stastny01@gmail.com>
2019-02-05 14:39:07 +0100

    Cache finally works
    Tested on chromium only

--- web/offlineServiceWorker.js
+++ web/offlineServiceWorker.js
@@ -1,7 +1,8 @@
 // NOTE: This has to be placed in / (root directory), 
 // Service workers can only work in current directory (or subdirectories).
 // By placing this file here, we can ensure it'll work for every page.
-// TODO: This doesn't work
+
+// https://jakearchibald.com/2014/offline-cookbook/
 
 self.addEventListener('install', function (e) {
     e.waitUntil(
@@ -9,11 +10,26 @@ self.addEventListener('install', function (e) {
             return cache.addAll([
                 '/',
                 '/student',
-                '/js/ui.js',
-                '/js/buttonSwitch.js',
+                '/js/studentRefresh.js',
+                '/js/subjectDetails.js',
+                '/js/switchTimetableDisplay.js',
                 '/css/pure-custom.css',
                 '/css/pure-grids-responsive-min.css',
                 '/css/pure-min.css',
-                '/css/pure-sidemenu.css'])
+                '/css/google-material-design-icons.css',
+                '/other/google-material-design-icons.woff2',
+                '/other/signout-icon.png',
+                '/css/pure-sidemenu.css',
+                '/favicon.ico'])
         }))
 });
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(async function() {
+      try {
+        return await fetch(event.request);
+      } catch (err) {
+        return caches.match(event.request);
+      }
+    }());
+  });
