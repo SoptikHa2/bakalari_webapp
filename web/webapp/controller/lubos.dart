c081bb3829a71c5977dcc98d40c49b1c77d7793b
Petr Stastny <petr.stastny01@gmail.com>
2019-03-13 18:33:30 +0000

    Commented some student controllers

--- web/webapp/controller/lubos.dart
+++ web/webapp/controller/lubos.dart
@@ -6,6 +6,7 @@ import 'package:stream/stream.dart';
 import '../view/general/lubosView.rsp.dart';
 
 class GeneralLubosController {
+  /// I'd rather not comment this one
   static Future showLubosCites(HttpConnect connect) {
     var file = File(join(dirname(Platform.script.toFilePath()), "lubos.csv"));
     var cites = List<dynamic>();
--- web/webapp/controller/schoolList.dart
+++ web/webapp/controller/schoolList.dart
@@ -3,6 +3,8 @@ import 'package:stream/stream.dart';
 import '../config.dart';
 import '../tools/db.dart';
 class SchoolListController {
+  /// User sent some school name. Find top X (default: 5) schools with their
+  /// URLs and display them in CSV format.
   static void returnSchoolListInCsvByQuery(HttpConnect connect) async {
     var query = '';
     if (connect.request.uri.queryParameters.keys.contains('query')) {
