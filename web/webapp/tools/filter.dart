4b3d0ecd48a7f2672f2476a3a01bb9c307f1ee1f
Petr Stastny <petr.stastny01@gmail.com>
2019-03-13 18:18:49 +0000

    Commented filter.dart

--- web/webapp/tools/filter.dart
+++ web/webapp/tools/filter.dart
@@ -2,11 +2,16 @@ import 'package:stream/stream.dart';
 import '../config.dart';
 import 'db.dart';
 
+/// Every request goes here
 class Filter {
+  /// Process a request. Log it if needed and sometimes redirect to /shutdown.
   static Future process(
       HttpConnect connect, Future chain(HttpConnect conn)) async {
+    // Remove all slashes. The point is to remove trailing slashes, but we
+    // can as well remove all of them.
     var path = connect.request.uri.path.replaceAll('/', '');
 
+    // If user tries to connect to a static file, let it through (without logging)
     if (path.endsWith('.js') ||
         path.endsWith('.css') ||
         path.endsWith('.jpg') ||
@@ -16,17 +21,20 @@ class Filter {
       return chain(connect);
     }
 
-    // Log everything except for .js, .css, .jpg, .ico, .txt, .csv requests
+    // Log user visit
     DB.logAccess(connect.request);
 
+    // If user tries to reach /shutdown, let him go there
     if (path.endsWith('shutdown')) {
       return chain(connect);
     }
 
+    // If there is defined shutdown, forward user to /shutdown
     if (Config.siteShutdownType != ShutdownTemplate.None) {
       return connect.redirect('/shutdown');
     }
 
+    // If none of the situations above happened, continue
     return chain(connect);
   }
 }
